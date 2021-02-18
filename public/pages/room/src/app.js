2365ac4dc6e967e7aa7dad42c60b615f9caf735f
lucassantosf <lucas46em@gmail.com>
2021-02-18 08:49:00 -0300

    Day 1 - server socket.io and frontend app

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -11,11 +11,20 @@ const recordClick = function (recorderBtn) {
 const onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const room = urlParams.get('room');
-  console.log('this is the room', room)
+  //console.log('this is the room', room)
 
-  const recorderBtn = document.getElementById('record')
-  recorderBtn.addEventListener('click', recordClick(recorderBtn))
+  //const recorderBtn = document.getElementById('record')
+  //recorderBtn.addEventListener('click', recordClick(recorderBtn))
+  const socketUrl = 'http://localhost:3000'
+  const socketBuilder = new SocketBuilder({socketUrl})
+  const view = new View()
+  const media = new Media()
+  const deps = {view,media,room,socketBuilder}
 
+  Business.initialize(deps)
+  //view.renderVideo({ userId: 'teste01', url: 'https://media.giphy.com/media/L3KIF2DW5POtXZsErD/giphy.mp4' })
+  //view.renderVideo({ userId: 'teste01', isCurrentId: true, url: 'https://media.giphy.com/media/L3KIF2DW5POtXZsErD/giphy.mp4' })
+  //view.renderVideo({ userId: 'teste01',  url: 'https://media.giphy.com/media/L3KIF2DW5POtXZsErD/giphy.mp4' })
 }
 
 window.onload = onload
